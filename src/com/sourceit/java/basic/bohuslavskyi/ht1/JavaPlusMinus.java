e6624112210bfa54d3eec82a948b01d6c55f5bb4
Макс <@192.168.1.5>
2015-04-27 13:37:57 +0300

    Initial commit

---
+++ src/com/sourceit/java/basic/bohuslavskyi/ht1/JavaPlusMinus.java
@@ -0,0 +1,38 @@
+package com.sourceit.java.basic.bohuslavskyi.ht1;
+
+public class JavaPlusMinus {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Преимущества:\n");
+		System.out.println("1. Переносимость.");
+		System.out.println("2. Безопасность.");
+		System.out.println("3. Простота.");
+		System.out.println("4. Надежность.");
+		System.out.println("5. Общедоступность.");
+		System.out.println("6. Многопоточность.");
+		System.out.println("7. Интерпретируемость.");
+		System.out.println("8. Рефлективность.");
+		System.out.println("9. Богатая библиотека.");
+		System.out.println("10. Объектно-ориентированный язык.");
+		System.out.println("11. Весь код в одном файле.");
+		System.out.println("12. Стандартизация на уровне двоичных кодов.");
+		System.out.println("13. Многообразие типов приложений.");
+		System.out.println("14. Невозможно создавать функции, не являющиеся методами какого-либо класса.");
+		System.out.println("15. Язык создан программистами для программистов.");
+		System.out.println("16. Непотопляемость в мире политики.");
+		System.out.println("17. Устойчивый успех на микрочипах.");
+		System.out.println("18. Самодокументируемый код.");
+		System.out.println("19. Способность адаптации языка к высокой скорости изменений в компьютерном мире.");
+		System.out.println("20. Отсутствие прямой работы с памятью.");
+		System.out.println("21. Уменьшение количества ошибок.\n");
+		
+		System.out.println("Недостатки:\n");
+		System.out.println("1. Ресурсоемкий и медлительный язык.");
+		System.out.println("2. Сложность синтаксиса вложенных классов.");
+		System.out.println("3. Требует наличие JRE у пользователя.");
+		System.out.println("4. Для эффективного выполнения Java-приложений, система пользователя должна обладать достаточно мощными вычислительными ресурсами.");
+
+	}
+
+}
